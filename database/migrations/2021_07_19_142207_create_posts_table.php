ee52e1966cc20c7b3ea7aec083b97a77d38c4d7d
ryota5523 <r.kikuchi0523@gmail.com>
2021-11-27 00:28:33 +0900

    imageserviceの編集

--- database/migrations/2021_07_19_142207_create_posts_table.php
+++ database/migrations/2021_07_19_142207_create_posts_table.php
@@ -23,7 +23,7 @@ class CreatePostsTable extends Migration
             $table->timestamps();
 
             //user_idに外部キー制約
-            $table->foreign('user_id')->references('id')->on('users');
+            // $table->foreign('user_id')->references('id')->on('users');
         });
     }
 
