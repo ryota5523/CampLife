8afcbd7a043970ce3957f9365508db9e58537f11
ryota5523 <r.kikuchi0523@gmail.com>
2021-11-26 23:03:33 +0900

    imageserviceの編集

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -54,18 +54,18 @@
                         
                       @else
                         <li class="nav-item dropdown pl-2">
-                        @if(!empty($post->iconfile))
-                        <a id="navbarDropdown" class="header-icon" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                          <img src="{{ Storage::disk('s3')->url('users/'. auth()->user()->iconfile) }}">
-                          </a>
-                          @else
+                        @if(empty($post->iconfile))
                           <a id="navbarDropdown" class="" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                             <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi bi-person-circle icon" viewBox="0 0 16 16">
                               <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z"/>
                               <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"/>
                             </svg>
                           </a>
-                          @endif
+                        @else
+                          <a id="navbarDropdown" class="header-icon" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                          <img src="{{ Storage::disk('s3')->url('users/'. auth()->user()->iconfile) }}">
+                          </a>
+                        @endif
 
                           <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
                             <a class="dropdown-item" href="{{ url(auth()->user()->id) }}"
